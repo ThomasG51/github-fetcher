ead2989b0244b76b1aa5ed3fe5fcec3fabc38cd7
Thomas George <private@thomas-george.com>
2022-05-27 09:40:11 +0200

    feat: setup search bar

--- GitHub-Fetcher/View/RepositoryViewController.swift
+++ GitHub-Fetcher/View/RepositoryViewController.swift
@@ -14,12 +14,15 @@ class RepositoryViewController: UIViewController {
     
     // MARK: - IBOutlet
     
+    @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var repositoryTableView: UITableView!
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchBar.delegate = self
         
         repositoryTableView.delegate = self
         repositoryTableView.dataSource = self
@@ -37,5 +40,13 @@ extension RepositoryViewController: UITableViewDelegate, UITableViewDataSource {
         let cell = repositoryTableView.dequeueReusableCell(withIdentifier: repositoryCellID, for: indexPath) as! RepositoryTableViewCell
         cell.setupCell(name: "Setup/TV", language: "Swift", description: "Setup repository table view", stars: 100)
         return cell
+    }
+}
+
+// MARK: - UISearchBarDelegate
+
+extension RepositoryViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
     }
 }
