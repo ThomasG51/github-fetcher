022a7828a5159a951c2de5541746641e8e1d3bf5
Thomas George <private@thomas-george.com>
2022-05-27 09:50:08 +0200

    feat: create repository model

---
+++ GitHub-Fetcher/Model/Repositories.swift
@@ -0,0 +1,27 @@
+//
+//  Repositories.swift
+//  GitHub-Fetcher
+//
+//  Created by Thomas George on 27/05/2022.
+//
+
+import Foundation
+
+struct Repositories: Codable {
+    let items: [Repository]
+    struct Repository: Codable {
+        enum CodingKeys: String, CodingKey {
+            case id
+            case name = "full_name"
+            case description
+            case language
+            case stars = "stargazers_count"
+        }
+
+        let id: Int
+        let name: String
+        let description: String
+        let language: String
+        let stars: Int
+    }
+}
