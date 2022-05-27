110a9301f1ce4ae564592b9bd3d6f5ec411f9855
Thomas George <private@thomas-george.com>
2022-05-27 09:18:12 +0200

    feat: setup repository cell

---
+++ GitHub-Fetcher/View/Cell/RepositoryTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  RepositoryTableViewCell.swift
+//  GitHub-Fetcher
+//
+//  Created by Thomas George on 27/05/2022.
+//
+
+import UIKit
+
+class RepositoryTableViewCell: UITableViewCell {
+    @IBOutlet var name: UILabel!
+    @IBOutlet var language: UILabel!
+    @IBOutlet var repoDescription: UILabel!
+    @IBOutlet var stars: UILabel!
+
+    func setupCell(name: String, language: String, description: String, stars: Int) {
+        self.name.text = name
+        self.language.text = language
+        self.repoDescription.text = description
+        self.stars.text = String(stars)
+    }
+}
---
+++ GitHub-Fetcher/View/RepositoryViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RepositoryViewController.swift
+//  GitHub-Fetcher
+//
+//  Created by Thomas George on 27/05/2022.
+//
+
+import UIKit
+
+class RepositoryViewController: UIViewController {
+    // MARK: - Property
+    
+    let repositoryCellID = "RepositoryCellIdentifier"
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
